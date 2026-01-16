2d962b0eb91ebcd8cd0f1527e62f53946d6edfb5
ahmadshakib770 <ahmadshakib770@gmail.com>
2026-01-16 17:05:38 +0600

    fixv1

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import planRoutes from './routes/plans.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config({ path: join(__dirname, '..', '.env') });
+dotenv.config({ path: join(__dirname, '.env') });
 
 const app = express();
 
